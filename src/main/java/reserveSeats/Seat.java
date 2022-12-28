21407b46e99f60e9febda987ff6d3361fd652f78
osayijoy <osayijoy16@gmail.com>
2022-12-28 10:58:15 -0800

    first commit

---
+++ src/main/java/reserveSeats/Seat.java
@@ -0,0 +1,37 @@
+package collectionsFrameWork;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Seat{
+    private final String seatNumber;
+    private boolean reserved = false;
+
+    public Seat(String seatNumber){
+        this.seatNumber = seatNumber;
+    }
+
+
+    public boolean reserve(){
+        if(!this.reserved){
+            this.reserved = true;
+            System.out.println("Seat " + seatNumber + " reserved");
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public boolean cancel(){
+        if(this.reserved){
+            this.reserved = false;
+            System.out.println("Reservation of seat " + seatNumber + " is cancelled");
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+}
